b7a8716981ceb32128401ec84e462213e1bb4630
Lee seung won <51774867+ujunja@users.noreply.github.com>
2022-06-16 16:12:04 +0900

    Update app.js

--- hello-world/app.js
+++ hello-world/app.js
@@ -26,7 +26,7 @@ exports.lambdaHandler = async (event, context) => {
         }
     } catch (err) {
         console.log(err);
-        console.log('check3');
+        console.log('check-please!!!');
         return err;
     }
 
